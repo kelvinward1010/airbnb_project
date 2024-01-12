6763e797813d20cf1fdf53cc07dcfbd802b675cf
Kelvin Ward <kelvinward1010@gmail.com>
2024-01-13 03:12:44 +0700

    update

---
+++ app/components/listings/ListingCard.tsx
@@ -0,0 +1,87 @@
+'use client'
+
+import useCountries from "@/app/hooks/useCountries";
+import { Listing, Reservation, User } from "@prisma/client";
+import { format } from "date-fns";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useCallback, useMemo } from "react";
+
+interface ListingCardProps{
+    data?: Listing;
+    currentUser: User;
+    reservations?: Reservation;
+    onAction?: (id: string) => void;
+    disabled?: boolean;
+    actionLabel?: string;
+    actionId?: string;
+}
+
+export const ListingCard: React.FC<ListingCardProps> = ({
+    data,
+    currentUser,
+    reservations,
+    onAction,
+    disabled,
+    actionId = '',
+    actionLabel,
+}) => {
+
+    const router = useRouter();
+    const { getByValue } = useCountries();
+
+    const location = getByValue(data?.locationValue as string);
+
+    const handleCancel = useCallback((e: React.MouseEvent<HTMLButtonElement>) =>{
+        e.stopPropagation();
+        if(disabled) return;
+
+        onAction?.(actionId);
+
+    },[onAction, actionId, disabled])
+
+    const price = useMemo(() => {
+        if(reservations){
+            return reservations.totalPrice;
+        }
+
+        return data?.price;
+    },[])
+
+    const reservation = useMemo(() => {
+        if(!reservations) return null;
+
+        const start = new Date(reservations.startDate);
+        const end = new Date(reservations.endDate);
+
+        return `${format(start, `PP`)} - ${format(end, `PP`)}`;
+    },[reservations])
+
+    return (
+        <div
+            onClick={() => router.push(`/listings/${data?.id}`)}
+            className="col-span-1 cursor-pointer group"
+        >
+            <div className="flex flex-col gap-2 w-full">
+                <div className="aspect-square w-full relative overflow-hidden rounded-xl">
+                    <Image
+                        fill
+                        alt="Listing"
+                        src={data?.image as string}
+                        className="
+                            w-full
+                            h-full
+                            object-cover
+                            group-hover:scale-110
+                            transition
+                        "
+                    />
+
+                    <div className="absolute top-3 right-3">
+                        :3
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,44 @@
+import { User } from "@prisma/client";
+import getCurrentUser from "./actions/getCurrentUser";
+import getListings, { IListingsParams } from "./actions/getListings";
+import { ClientOnly } from "./components/ClientOnly";
+import { Container } from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import { ListingCard } from "./components/listings/ListingCard";
+
+interface HomeProps {
+    searchParams: IListingsParams
+};
 
 
-function HomePage() {
-    return null;
+const HomePage = async ({ searchParams }: HomeProps) => {
+
+    const listings = await getListings(searchParams);
+    const currentUser = await getCurrentUser();
+
+    if(listings.length === 0){
+        return (
+            <ClientOnly>
+                <EmptyState showReset/>
+            </ClientOnly>
+        )
+    }
+
+    return (
+        <ClientOnly>
+            <Container>
+                <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8 ">
+                    {listings.map((listing, idx) => (
+                        <ListingCard
+                            currentUser={currentUser as User}
+                            data={listing}
+                            key={listing.id}
+                        />
+                    ))}
+                </div>
+            </Container>
+        </ClientOnly>
+    );
 }
 
 export default HomePage
