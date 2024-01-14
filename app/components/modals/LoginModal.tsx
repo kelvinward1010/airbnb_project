62f21c20cd6765e44d9a7ef01c1acdb746e99313
Kelvin Ward <kelvinward1010@gmail.com>
2024-01-14 09:30:39 +0700

    update

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -129,9 +129,9 @@ export function LoginModal() {
                 "
             >
                 <div className="flex flex-row justify-center items-center gap-2">
-                    <div>
+                    <p>
                         Don't have an account?
-                    </div>
+                    </p>
                     <div
                         onClick={toggle}
                         className="text-neutral-800 cursor-pointer hover:underline"
